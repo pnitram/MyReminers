95dfb8b7db66860e6046d98f8b908f8737eeb35c
pnitram <martin.pedersen@me.com>
2017-09-15 14:59:30 +0200

    Initial commit

---
+++ app/src/main/java/no/martinpedersen/myreminers/MyRemindersActivity.java
@@ -0,0 +1,13 @@
+package no.martinpedersen.myreminers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyRemindersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_reminders);
+    }
+}
