92702846384bdfefdbf733bc729beb6e0b2788f6
pnitram <martin.pedersen@me.com>
2017-10-01 17:35:41 +0200

    Added Arrayadapter

--- app/src/main/java/no/martinpedersen/myreminers/MyRemindersActivity.java
+++ app/src/main/java/no/martinpedersen/myreminers/MyRemindersActivity.java
@@ -2,12 +2,29 @@ package no.martinpedersen.myreminers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyRemindersActivity extends AppCompatActivity {
+
+    private ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_reminders);
+        mListView = (ListView) findViewById(R.id.my_reminders_list_view);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                R.layout.my_reminders_row,
+                R.id.row_text,
+                new String[]{"first record", "second record", "third record"}
+        );
+
+        mListView.setAdapter(arrayAdapter);
     }
 }
