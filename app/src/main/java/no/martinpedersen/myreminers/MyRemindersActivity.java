4e15922a2bae00a4378aac60accebcff2fa482df
pnitram <martin.pedersen@me.com>
2017-10-10 18:41:03 +0200

    Switched from arrayadapter to simplecursoradapter to use sqlite database

--- app/src/main/java/no/martinpedersen/myreminers/MyRemindersActivity.java
+++ app/src/main/java/no/martinpedersen/myreminers/MyRemindersActivity.java
@@ -1,5 +1,6 @@
 package no.martinpedersen.myreminers;
 
+import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,20 +17,56 @@ import java.util.List;
 public class MyRemindersActivity extends AppCompatActivity {
 
     private ListView mListView;
+    private RemindersDbAdapter mDbAdapter;
+    private RemindersSimpleCursorAdapter mCursorAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_reminders);
         mListView = (ListView) findViewById(R.id.my_reminders_list_view);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
+        mListView.setDivider(null);
+
+        mDbAdapter = new RemindersDbAdapter(this);
+        mDbAdapter.open();
+
+        //mDbAdapter.deleteAllReminders();
+        //mDbAdapter.createReminder("Dette er en test",true);
+        //mDbAdapter.createReminder("enda en test",false);
+
+        Cursor cursor = mDbAdapter.fetchAllReminders();
+
+        //from colums defined in db
+        String[] from = new String[]{
+                RemindersDbAdapter.COL_CONTENT
+        };
+
+        //to the ids of views in the layout
+        int[] to = new int[]{
+                R.id.row_text
+        };
+
+        mCursorAdapter = new RemindersSimpleCursorAdapter(
+                //context
+                MyRemindersActivity.this,
+                //the layout if the row
                 R.layout.my_reminders_row,
-                R.id.row_text,
-                new String[]{"first record", "second record", "third record"}
+                //cursor
+                cursor,
+                //from colums defined in db
+                from,
+                //to the ids of views in  the layout
+                to,
+                //flag - not used
+                0
         );
 
-        mListView.setAdapter(arrayAdapter);
+        //the cursoradapter (controller) is now updating the listView (view)
+        //with data from the db (model)
+        mListView.setAdapter(mCursorAdapter);
+
+
     }
 
     @Override
